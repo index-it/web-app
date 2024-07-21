83a2e150e282258f3befa70da712e5a34e1f4abb
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2024-07-21 13:56:58 +0200

    fix: sidebar

--- components/ui/layout/sidebar.tsx
+++ components/ui/layout/sidebar.tsx
@@ -115,7 +115,7 @@ export function Sidebar() {
           variant="ghost"
           size="sm"
           onClick={() => setDesktopCollapsed(false)}
-          className="hidden sm:inline z-10 bg-background my-4 ml-4 fixed"
+          className="hidden sm:inline z-10 bg-background my-4 ml-4 fixed bottom-0"
         >
           <Icon icon="ph:sidebar-simple" className="size-5" />
         </Button>
